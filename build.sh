3678351e35c89c712af2a4d90f23b94a27c39dff
Simon Wood <simon.wood@smarsh.com>
2024-06-03 12:05:32 +0100

    NFO-1524

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+if [ $# -lt 1 ]; then
+        echo "Usage: $0 {version}"
+        exit 1
+fi
+version=$1
+
+#CGO_ENABLED=0 go build -o src/safe .
+
+cat > config/final.yml << EOF
+name: vault-bbr
+blobstore:
+  provider: local
+  options:
+    blobstore_path: $PWD/blobs
+EOF
+
+mkdir -p build
+bosh create-release --final --version=$version --tarball=build/vault-bbr-$version.tgz
+
+# remove all these pointless directories, we are creating a tarball
+rm -rf config/final.yml .dev_builds .final_builds dev_releases releases blobs
